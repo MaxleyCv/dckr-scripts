27ad64f9e8b521cca99e13aa65f9f9a33bf903f2
root <root@instance-2.europe-west3-c.c.marine-resource-313014.internal>
2021-05-20 22:53:53 +0000

    improved some security

--- serverside/runner.sh
+++ serverside/runner.sh
@@ -2,8 +2,24 @@ cd
 wget https://github.com/prometheus/prometheus/releases/download/v2.26.0/prometheus-2.26.0.linux-amd64.tar.gz
 tar -xzf prometheus-2.26.0.linux-amd64.tar.gz
 cd prometheus-2.26.0.linux-amd64/
-rm -f prometheus.yml
-wget https://raw.githubusercontent.com/damoklov/operation-systems/master/prometheus.yml
+echo "
+global:
+  scrape_interval: 15s
+
+scrape_configs:
+  - job_name: 'prometheus'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['localhost:9090']
+  - job_name: 'node'
+    scrape_interval: 10s
+    static_configs:
+      - targets: ['localhost:9100']
+  - job_name: 'spring_micrometer'
+    metrics_path: '/actuator/prometheus'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['localhost:8080']" > prometheus.yml
 ./prometheus &
 
 cd
